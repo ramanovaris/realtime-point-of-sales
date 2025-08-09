1ae696819fd556b9fe4e917349f501ed73b318a9
Rama Novaris <ramaanovariss@gmail.com>
2025-08-10 02:15:21 +0800

    feat(dashboard): Sidebar Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { DarkModeToggle } from "@/components/common/dark-mode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button className="dark:bg-amber-400">Hello</Button>
-      <DarkModeToggle />
+    <div className="bg-muted space-y-4 flex flex-col items-center justify-center h-screen">
+      <h1 className="font-semibold text-4xl">Welcome Rama Novaris</h1>
+      <Link href={"/admin"}>
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
