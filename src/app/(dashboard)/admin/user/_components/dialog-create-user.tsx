08ad10d06ddd7484cad1568ff821ebebebda57c7
Rama Novaris <ramaanovariss@gmail.com>
2025-08-17 01:25:28 +0800

    feat(user-management): Input File Handle

--- src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -21,11 +21,12 @@ import {
 } from "@/validations/auth-validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
-import { startTransition, useActionState, useEffect } from "react";
+import { startTransition, useActionState, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { createUser } from "../actions";
 import FormSelect from "@/components/common/form-select";
+import FormImage from "@/components/common/form-image";
 
 export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const form = useForm<CreateUserForm>({
@@ -36,10 +37,14 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
   const [createUserState, createUserAction, isPendingCreateUser] =
     useActionState(createUser, INITIAL_STATE_CREATE_USER);
 
+  const [preview, setPreview] = useState<
+    { file: File; displayUrl: string } | undefined
+  >(undefined);
+
   const onSubmit = form.handleSubmit(async (data) => {
     const formData = new FormData();
     Object.entries(data).forEach(([key, value]) => {
-      formData.append(key, value);
+      formData.append(key, key === "avatar_url" ? preview!.file ?? "" : value);
     });
 
     startTransition(() => {
@@ -57,6 +62,7 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
     if (createUserState?.status === "success") {
       toast.success("Create User Success");
       form.reset();
+      setPreview(undefined);
       document.querySelector<HTMLButtonElement>('[data-state="open"')?.click();
       refetch();
     }
@@ -82,6 +88,13 @@ export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
             label="Email"
             placeholder="Insert email here"
             type="email"
+          />
+          <FormImage
+            form={form}
+            name="avatar_url"
+            label="Avatar"
+            preview={preview}
+            setPreview={setPreview}
           />
           <FormSelect
             form={form}
