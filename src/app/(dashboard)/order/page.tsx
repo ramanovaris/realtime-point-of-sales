b6aab019124cbfd9466536547e7936f657bd408b
Rama Novaris <ramaanovariss@gmail.com>
2025-09-24 10:50:53 +0800

    feat(order-management): order list

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "Rama Cafe | Order Management",
+};
+
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
