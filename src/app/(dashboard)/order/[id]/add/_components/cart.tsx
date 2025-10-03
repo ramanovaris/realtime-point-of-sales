c519b5dbce5cf77ce3da136b47f6ad4b1764f302
Rama Novaris <ramaanovariss@gmail.com>
2025-10-03 23:49:28 +0800

    feat(order-management): Add To Cart

--- src/app/(dashboard)/order/[id]/add/_components/cart.tsx
+++ src/app/(dashboard)/order/[id]/add/_components/cart.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import useDebounce from "@/hooks/use-debounce";
+import { convertIDR } from "@/lib/utils";
+import { Cart } from "@/types/order";
+import { Menu } from "@/validations/menu-validation";
+import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
 
 export default function CartSection({
   order,
+  carts,
+  setCarts,
+  onAddToCart,
 }: {
   order:
     | {
@@ -13,7 +24,18 @@ export default function CartSection({
       }
     | undefined
     | null;
+  carts: Cart[];
+  setCarts: Dispatch<SetStateAction<Cart[]>>;
+  onAddToCart: (item: Menu, type: "decrement" | "increment") => void;
 }) {
+  const debounce = useDebounce();
+
+  const handleAddNote = (id: string, notes: string) => {
+    setCarts(
+      carts.map((item) => (item.menu_id === id ? { ...item, notes } : item))
+    );
+  };
+
   return (
     <Card>
       <CardContent className="space-y-4">
@@ -33,6 +55,65 @@ export default function CartSection({
             </div>
           </div>
         )}
+        <Separator />
+        <div className="space-y-4">
+          <h3 className="text-lg font-semibold">Cart</h3>
+          {carts.length > 0 ? (
+            carts?.map((item: Cart) => (
+              <div className="space-y-2" key={item.menu_id}>
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      src={item.menu.image_url as string}
+                      alt={item.menu.name}
+                      width={30}
+                      height={30}
+                      className="rounded"
+                    />
+                    <div>
+                      <p className="text-sm">{item.menu.name}</p>
+                      <p className="text-xs text-muted-foreground">
+                        {convertIDR(item.total / item.quantity)}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="text-sm">{convertIDR(item.total)}</p>
+                </div>
+                <div className="flex items-center gap-4 w-full">
+                  <Input
+                    placeholder="Add Note"
+                    className="w-full"
+                    onChange={(e) =>
+                      debounce(
+                        () => handleAddNote(item.menu!.id, e.target.value),
+                        500
+                      )
+                    }
+                  />
+                  <div className="flex items-center gap-4">
+                    <Button
+                      variant={"outline"}
+                      className="font-semibold cursor-pointer"
+                      onClick={() => onAddToCart(item.menu!, "decrement")}
+                    >
+                      -
+                    </Button>
+                    <p className="font-semibold">{item.quantity}</p>
+                    <Button
+                      variant={"outline"}
+                      className="font-semibold cursor-pointer"
+                      onClick={() => onAddToCart(item.menu!, "increment")}
+                    >
+                      +
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="text-sm">No item in cart</p>
+          )}
+        </div>
       </CardContent>
     </Card>
   );
